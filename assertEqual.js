7c4017dbd2d6124662d8d62ee547e6662fc89459
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-05-22 17:04:53 -0300

    create assert equal function

---
+++ assertEqual.js
@@ -0,0 +1,18 @@
+//function implementation
+const assertEqual = function(actual, expected) {
+  //compare two values and print message
+if (actual === expected) {
+  console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+}
+else {
+  console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+}
+};
+
+//TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual('shoes', 65);
+assertEqual('Sunday', 'Sunday');
+assertEqual(23, 23);
+assertEqual(24, 21);
