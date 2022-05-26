28495d60dd82e7589e51fab821fd6959085d1dae
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-05-26 14:29:17 -0300

    message

--- without.js
+++ without.js
@@ -60,5 +60,5 @@ const without = function(arraySource, itemsToRemove) {
 
 
 //TEST CASES BELOW
-console.log(without([1, 2, 3], [1])); // => [2, 3]
-console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
+//console.log(without([1, 2, 3], [1])); // => [2, 3]
+//console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
