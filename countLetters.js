2e39b73c10f5116b6fd68595f73f3d12f0487859
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-05-27 15:41:55 -0300

    create function to count letters in a string and return object displaying each letter present and its count

---
+++ countLetters.js
@@ -0,0 +1,33 @@
+//copy assert equal fx
+//function implementation
+const assertEqual = function(actual, expected) {
+  //compare two values and print message
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+//end of copy of assert equal fx
+
+//NEW function below
+const countLetters = function(string) {
+  let result = {};
+  //result object
+  //for of loop can be used with strings
+  for (const letter of string) {
+    //if a string is not input
+    if (letter !== " ") {
+      //if its an additional occurance of that letter add one to count
+      if (result[letter]) {
+        result[letter] += 1;
+        //else if it hasnt been counted yet, adds a one count
+      } else {
+        result[letter] = 1;
+      }
+    }
+  }
+  return result;
+};
+//test case:
+//console.log(countLetters('mississippi half step'));
