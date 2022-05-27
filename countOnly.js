a87c331e52733d68dadf4e26fcfc9c6f32d7be19
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-05-26 23:27:25 -0300

    finalized code for countOnly function

--- countOnly.js
+++ countOnly.js
@@ -14,26 +14,43 @@ const assertEqual = function(actual, expected) {
 
 const countOnly = function(allItems, itemsToCount) {
 //function body
+  const results = {};
+  //scan entire list with for of loop
+  for (const item of allItems) {
+  //increment counter for each item
+  //set a property with that string key to:
+  //the value that was already there (or zero if nothing there) with 1 added to it
+  //Add an if condition to only increment our count in results if the item is found in the itemsToCount object.
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      } //close else
+    } //close if
+    console.log(item);
+  } //close for of loop
 
-}
+  return results;
+};     // close count only funtion
 
 
-//TEST CODE FOR BOTTOM OF CODE
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
+//TEST CASE FOR BOTTOM OF CODE
+// const firstNames = [
+//   "Karl",
+//   "Salima",
+//   "Agouhanna",
+//   "Fang",
+//   "Kavith",
+//   "Jason",
+//   "Salima",
+//   "Fang",
+//   "Joe"
+// ];
 
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+// const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
 
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+// assertEqual(result1["Jason"], 1);
+// assertEqual(result1["Karima"], undefined);
+// assertEqual(result1["Fang"], 2);
+// assertEqual(result1["Agouhanna"], undefined);
