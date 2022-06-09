076b2bb2ddce235b8c4bc737d10893026bd7b201
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-06-08 23:06:46 -0300

    refactor eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,3 @@
-const assertEqual = function(actual, expected) {
-  //compare two values and print message
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-//function that takes in two arrays
 const eqArrays = function(array1, array2) {
   //if arrays are not the same length return false
   if (array1.length !== array2.length) {
@@ -23,8 +13,5 @@ const eqArrays = function(array1, array2) {
   //if above cases are not relevant, return true
   return true;
 };
-//test cases
-//console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-//console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
-//console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-//console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+
+module.exports = eqArrays;
