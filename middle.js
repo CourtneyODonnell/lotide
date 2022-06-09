b999bed2ccf737a3082fdffc422f13a4cb00e093
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-06-09 16:45:32 -0300

    refactor middle and add middleTest to lotide

--- middle.js
+++ middle.js
@@ -1,45 +1,3 @@
-//below code is pasted from previous exercise
-//below code pasted from previous eqArrays.js assignment:
-const assertEqual = function(actual, expected) {
-  //compare two values and print message
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-//function that takes in two arrays
-const eqArrays = function(array1, array2) {
-  //if arrays are not the same length return false
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  //if arrays were equal lengths, iterate through with for loop
-  for (let i = 0; i < array1.length; i++) {
-    //if not exactly equal arrays return false
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  //if above cases are not relevant, return true
-  return true;
-};
-//END OF above code pasted from previous eqArrays.js assignment
-//NEW code below for assertArraysEqual.js
-
-const assertArraysEqual = function(actualInput, ExpectedInput) {
-  if (eqArrays(actualInput, ExpectedInput) === true) {
-    //if input arrays evaluate to true through the eqarrays fx, log:
-    console.log(`✅ Assertion Passed: ${actualInput} === ${ExpectedInput}`);
-  } else {
-    //otherwise, log:
-    console.log(`🛑 Assertion Failed: ${actualInput} !== ${ExpectedInput}`);
-  }
-};
-//DONE above code pasted from prev exercises
-/* -------------------------------------- */
-//BEGIN OF NEW CODE FOR CURRENT ASSIGNMENT
 //function to take in array and return middle element or elements
 const middle = function(array) {
   //array to hold answer
@@ -65,10 +23,5 @@ const middle = function(array) {
     return newArray;
   }
 };
-/* test cases
-console.log(middle([1, 2, 3]))
-console.log(middle([1, 2, 3, 4, 5]))
 
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
-*/
+module.exports = middle;
