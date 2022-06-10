adafaa40b67b86fd7003cdf6f1a442fb1c453623
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-06-10 12:27:26 -0300

    refactor middleTest and tailTest to use chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,13 +1,29 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-//test case 1
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+describe("#tail", () => {
+  it("returns 1st element as 'Lighthouse' ( ['Lighthouse', 'Labs'] ) for ['Hello', 'Lighthouse', 'Labs']", () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(result[0], 'Lighthouse');// ensure first element is "Lighthouse"
+  });
 
-// Test Case 2:
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+  it("returns 2nd element as 'Labs' for ( ['Lighthouse', 'Labs'] ) for ['Hello', 'Lighthouse', 'Labs']", () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(result[1], "Labs"); // ensure second element is "Labs"
+  });
+
+  it("returns array length of 2 ( ['Lighthouse', 'Labs'] ) for ['Hello', 'Lighthouse', 'Labs']", () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(result.length, 2); // ensure we get back two elements
+  });
+
+  it("returns 3 for original array length of ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    assert.strictEqual(words.length, 3); //ensure original array still has 3 elements
+  }); 
+
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    assert.deepEqual(tail(words), ['Lighthouse', 'Labs']);
+  });   
+});
